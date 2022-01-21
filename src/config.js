c086127b51d8a3e5495ff281311dfd355da7e7ce
tomaszpankowski <pankowski.email@gmail.com>
2022-01-21 12:42:28 +0100

    modified routing

--- src/config.js
+++ src/config.js
@@ -1,16 +1,24 @@
 import {Redirect} from "react-router-dom";
+import Apparel from "./views/apparel";
 import Comingsoon from "./views/comingsoon";
 import Contact from "./views/contact";
 import Error from "./views/error";
+import Home from "./views/home";
 import Login from "./views/login";
 import PrivacyPolicy from "./views/privacy";
 import Register from "./views/register";
+import Repaint from "./views/repaint";
+import Tuneup from "./views/tuneup";
 
 const appCfg = {
     routes:[
-        {navItem:true, exact:true, path:"/",name:"Comingsoon",view:<Comingsoon/>},
+        {navItem:true, exact:true, path:"/",name:"Home",view:<Home/>},
+        {navItem:true, exact:true, path:"/tuneup",name:"Tune up",view:<Tuneup/>},
+        {navItem:true, exact:true, path:"/repaint",name:"Repaint",view:<Repaint/>},
+        {navItem:true, exact:true, path:"/apparel",name:"Apparel",view:<Apparel/>},
         {navItem:true, exact:true, path:"/contact",name:"Contact",view:<Contact/>},
         {navItem:true, exact:true, path:"/login",name:<span className="fa fa-user-circle"></span>,view:<Login backLink={{name:" Register now!",href:"/register"}}/>},
+        {navItem:false, exact:true, path:"/comingsoon",name:"Comingsoon",view:<Comingsoon/>},
         {navItem:false, exact:true, path:"/register",name:"Register",view:<Register backLink={{name:" Login now!",href:"/login"}}/>},
         {navItem:false, exact:false, path:"/error",name:"Error",view:<Error backLink={{name:"back to Home",href:"/"}}/>},
         {navItem:false, exact:true, path:"/privacy",name:"Privacy",view: <PrivacyPolicy backLink={{name:"back to Home",href:"/"}}/>},
