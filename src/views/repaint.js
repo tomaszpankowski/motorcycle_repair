d1e5cf38f110c1eea1f0b50eb6c2114d6e52975d
tomaszpankowski <pankowski.email@gmail.com>
2022-02-10 15:32:31 +0100

    layout done

--- src/views/repaint.js
+++ src/views/repaint.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import RepaintS1 from "../components/repaint-s1";
+import RepaintS2 from "../components/repaint-s2";
+import RepaintS3 from "../components/repaint-s3";
 
 class Repaint extends Component{
     render(){
         return(        
-            <Container fluid className="repaint-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <RepaintS1/>
+                <RepaintS2/>
+                <RepaintS3/>
             </Container>    
         );
     }
--- src/views/tuneup.js
+++ src/views/tuneup.js
@@ -1,11 +1,19 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import TuneupS1 from "../components/tuneup-s1";
+import TuneupS2 from "../components/tuneup-s2";
+import TuneupS3 from "../components/tuneup-s3";
+import TuneupS4 from "../components/tuneup-s4";
+
 
 class Tuneup extends Component{
     render(){
         return(        
-            <Container fluid className="tuneup-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <TuneupS1/>
+                <TuneupS2/>
+                <TuneupS3/>
+                <TuneupS4/>
             </Container>    
         );
     }
