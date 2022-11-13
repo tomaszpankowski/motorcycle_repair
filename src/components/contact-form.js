3e06bba16431eb71220969f6a48f6e6be6d93d26
TP <pankowski.tomasz@prokonto.pl>
2022-11-13 08:49:05 +0100

    fonts,og,gpdr

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -74,7 +74,7 @@ class ContactForm extends Component {
 
     render() {
         return (
-            <Container fluid className={"contact-s2 align-items-center p-0 py-5  d-flex minh-50vh " + this.props.classExt}>
+            <Container fluid className={"contact-s2 align-items-center p-0 py-5 border-top border-secondary d-flex minh-50vh " + this.props.classExt}>
                 <Row className="mx-auto text-center w-100">
                     <Col xs={11} md={6} className="mx-auto p-0 d-flex align-items-center">
                         <Form className="text-start w-100 border border-secondary p-4 rounded bg-dark opacity-9 shadow text-secondary fw-bold"
